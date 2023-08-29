2f7c8c8239c904acb83443bf99a7dca0657e1b9e
AsakuraMizu <asakuramizu111@gmail.com>
2023-08-30 01:29:25 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/// provide empty parse5 implementation to reduce bundled .js size
+
+// parse5-htmlparser2-tree-adapter
+export const adapter = () => {};
+
+// parse5
+export const parse = () => {};
+export const parseFragment = () => {};
+export const serializeOuter = () => {};
